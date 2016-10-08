8807c276ba44b7b95c6e98bccd878883ac4f6f67
Michael Washburn <mdw7326@rit.edu>
2016-10-08 11:01:00 -0400

    Remove mock data and fix defaults

--- openbrews/recipe/add/AddRecipeCtrl.js
+++ openbrews/recipe/add/AddRecipeCtrl.js
@@ -9,9 +9,20 @@
 
       var LOCAL_STORAGE_KEY = "recipesInStorage";
 
-      /* remove the fermentable at the given index */
-      $scope.deleteFermentable = function(index) {
-        $scope.recipe.fermentables.splice(index,1);
+      $scope.recipe = {
+        name: "",
+        style: "",
+        boilSize: 5,
+        boilSizeUnits: "Gal",
+        boilTime: 60,
+        estFermentationDays: 7,
+        secondaryTimeDays: 0,
+        mashEfficiency: 68,
+        fermentables: [],
+        hops: [],
+        yeasts: [],
+        others: [],
+        notes: []
       };
 
       /* Add a new fermentable */
@@ -28,14 +39,19 @@
         );
       };
 
+      /* remove the fermentable at the given index */
+      $scope.deleteFermentable = function(index) {
+        $scope.recipe.fermentables.splice(index,1);
+      };
+
       /* Add other ingredient. */
       $scope.addOther = function() {
         $scope.recipe.others.push({
-          name: "Orange Peel",
-          amount: 5.0,
+          name: "",
+          amount: 0,
           amountUnits: "oz",
           stage: "Boil",
-          addTime: 60
+          addTime: 0
         });
       };
 
@@ -44,21 +60,23 @@
         $scope.recipe.others.splice(index,1);
       };
 
-      /* remove the Hop at the given index */
-      $scope.deleteHop = function(index) {
-        $scope.recipe.hops.splice(index,1);
-      };
-
       /* Add a new Hop */
       $scope.addHop = function() {
         $scope.recipe.hops.push(
           {
             weight: 0,
+            weightUnits: "oz",
+            type: "Pellet",
             aa: 0,
             stage: "Boil",
-            addTime: 60
+            addTime: 0
           }
         );
+      };
+
+      /* remove the Hop at the given index */
+      $scope.deleteHop = function(index) {
+        $scope.recipe.hops.splice(index,1);
       };
 
       /* Delete the yeast at supplied index */
@@ -70,6 +88,7 @@
       $scope.addYeast = function() {
         $scope.recipe.yeasts.push(
           {
+            amount: 0,
             attenuation: 0,
             flocculation: "Medium-Low",
             amountUnits: "G"
@@ -87,76 +106,6 @@
         history.push($scope.recipe);
         localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(history));
         $state.go("recipes");
-      };
-
-      $scope.recipe = {
-        name: "Citra Pale Ale",
-        style: "American Pale Ale",
-        boilSize: 5,
-        boilSizeUnits: "Gal",
-        boilTime: 60,
-        estFermentationDays: 7,
-        secondaryTimeDays: 7,
-        mashEfficiency: 68,
-        fermentables: [
-          {
-            name: "Pale Liquid Extract",
-            method: "Extract",
-            weight: 9.00,
-            weightUnits: "Lbs",
-            addTime: 15,
-            ppg: 1.036,
-            srm: 8.0
-          }
-        ],
-        hops: [
-          {
-            name: "Citra",
-            type: "Pellet",
-            weight: 1,
-            weightUnits: "oz",
-            aa: 0.125,
-            stage: "Boil",
-            addTime: 60
-          },
-          {
-            name: "Citra",
-            type: "Pellet",
-            weight: 1,
-            weightUnits: "oz",
-            aa: 0.125,
-            stage: "Secondary",
-            addTime: 0
-          }
-        ],
-        yeasts: [
-          {
-            name: "WY1056 American Ale",
-            attenuation: 0.75,
-            flocculation: "Medium-Low",
-            amount: "13.5",
-            amountUnits: "G"
-          }
-        ],
-        others: [
-          {
-            name: "Orange Peel",
-            amount: 5.0,
-            amountUnits: "oz",
-            stage: "Boil",
-            addTime: 60
-          },
-          {
-            name: "Orange Peel",
-            amount: 5.0,
-            amountUnits: "oz",
-            stage: "Secondary",
-            addTime: 0
-          }
-        ],
-        notes: [
-          "Don't boil over!"
-        ]
       };
 
     }]);
