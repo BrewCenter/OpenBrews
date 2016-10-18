ce4a24bfc3fe73d2475ee46625ae4a10aee7082a
Michael Washburn <mdw7326@rit.edu>
2016-10-17 21:41:10 -0400

    Add initial fermentables endpoint

--- openbrews/recipe/BreweryDB.js
+++ openbrews/recipe/BreweryDB.js
@@ -4,18 +4,23 @@ angular.module('openbrews.breweryDB', [])
   var URL = 'http://api.brewerydb.com/v2';
 
   var STYLES_KEY = 'stylesInStorage';
+  var FERMENTABLES_KEY = 'fermentablesInStorage';
 
 
   /* sync all objects in the local cache with breweryDB */
   this.syncDB = function() {
     this.syncStyles();
+    this.syncFermentables();
   };
+
+  /////////////////////////////////////
+  // Beer Styles
+  /////////////////////////////////////
 
   /* 
    * Get all beer styles from the local cache.
    * Transform them to our required format, and return them.
    */
-
   var getStyles = function() {
     if (localStorage.getItem(STYLES_KEY)) {
       return JSON.parse(localStorage.getItem(STYLES_KEY));
@@ -47,9 +52,93 @@ angular.module('openbrews.breweryDB', [])
         localStorage.setItem(STYLES_KEY, JSON.stringify(styles));
         stylesInStorage = styles;
       }
-      return stylesInStorage;
     }, function failureCallback(response) {
+      console.log("Unable to sync styles.");
+    });
+  };
+
+  /////////////////////////////////////
+  // Fermentables
+  /////////////////////////////////////
+
+  /* a data map of pages to fermentables received from that page */
+  var fermentablesSyncing = [];
+
+  /* transform fermentable into an object we can use */
+  var parseFermentables = function(fermentables) {
+    fermentables = fermentables.map(function(fermentableObject) {//map to list of strings
+      return fermentableObject;
+    });
+    return fermentables;
+  };
+
+  /* 
+   * Get all beer styles from the local cache.
+   * Transform them to our required format, and return them.
+   */
+  var getFermentables = function() {
+    if (localStorage.getItem(FERMENTABLES_KEY)) {
+      return JSON.parse(localStorage.getItem(FERMENTABLES_KEY));
+    } else {
       return [];
+    }
+  };
+  this.getFermentables = getFermentables;
+
+  /*
+   * Called after all fermentables have been retrieved into the
+   * fermentablesSyncing Array
+   */
+  var onLastPage = function(numPages) {
+    fermentables = [];
+    for(var i = 1; i <= numPages; i++) {
+      fermentables = fermentables.concat(fermentablesSyncing[i])
+    }
+    localStorage.setItem(FERMENTABLES_KEY, JSON.stringify(fermentables));
+    console.log(fermentables);
+  };
+
+  /* 
+   * Get's the fermantables page #p. Transforms the response and saves it in
+   * the syncing array.
+   */
+  var getFermPage = function(p) {
+    return $http({
+      method: 'GET',
+      url: URL + '/fermentables',
+      params: {
+        key: $rootScope.config.BREWERY_DB_KEY,
+        p: p
+      }
+    }).then(function successCallback(response) {
+      var pageNum = response.data.currentPage;
+      var numPages = response.data.numberOfPages;
+      fermentablesSyncing[pageNum] = parseFermentables(response.data.data);
+      if(pageNum != numPages && p > 1) {//if it's not the first or last page
+        getFermPage(pageNum+1);
+      } else {
+        onLastPage(numPages);
+      }
+      return response;
+    }, function failureCallback(response) {
+      console.log("Unable to sync fermentables.");
+      return response;
+    });
+  };
+
+  this.syncFermentables = function() {
+    fermentablesSyncing = {};
+    /* make the first request to breweryDB to see if we need to sync */
+    getFermPage(1).then(function successCallback(response) {
+      var fermentablesInStorage = getFermentables();
+      var numFermentables = response.data.totalResults;
+
+      /* if there is a mismatch in lengths then we need to sync */
+      if(fermentablesInStorage.length != numFermentables) {
+        getFermPage(2);
+      }
+    }, function failureCallback(response) {
+      console.log("Unable to sync fermentables.");
     });
   };
 
