199820203807369df24925e89e830f68efb55f8d
Michael Washburn <mdw7326@rit.edu>
2016-11-01 09:43:34 -0400

    Add blank RecipeUtils service

---
+++ openbrews/recipe/RecipeUtils.js
@@ -0,0 +1,9 @@
+angular.module('openbrews.recipeUtils', [])
+.service('RecipeUtils', [function () {
+
+  /* test function to verify jasmine testing works */
+  this.test = function() {
+    return 1;
+  }
+
+}]);
