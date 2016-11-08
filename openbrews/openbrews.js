1bbdde8777632e9ee42b7675d271dd5738a381fa
Michael Washburn <mdw7326@rit.edu>
2016-11-08 12:07:53 -0500

    Fix mixpanel conditional

--- openbrews/openbrews.js
+++ openbrews/openbrews.js
@@ -1,5 +1,5 @@
-/*global
-  angular, window, cordova, StatusBar
+/*
+  window, cordova, StatusBar
 */
 (function() {
   'use strict';
@@ -45,12 +45,10 @@
           'platformVersion': ionic.Platform.version()
         });
       }
-
       // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
       // for form inputs)
       if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
         cordova.plugins.Keyboard.disableScroll(true);
-
       }
       if (window.StatusBar) {
         // org.apache.cordova.statusbar required
